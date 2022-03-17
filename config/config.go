df3b26e70315c0652cc29d555972ebfe180fdb4a
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-03-17 20:08:42 +0100

    fix error reading config file
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -35,8 +35,7 @@ type MirrorConfig struct {
 //Constructor new config file from file
 func NewConfig(configPath string, kubeconfig string) (MirrorConfig, error) {
 	//Read main config from the config file
-	var config = MirrorConfig{
-		ConfigFile:             configPath,
+	var conf = MirrorConfig{
 		Kubeconfig:             kubeconfig,
 		PullSecretTempFile:     "/tmp/pull-secret-temp.json",
 		RegistryCertPath:       "/etc/pki/ca-trust/source/anchors",
@@ -50,28 +49,35 @@ func NewConfig(configPath string, kubeconfig string) (MirrorConfig, error) {
 	}
 
 	if configPath == "" {
-		return config, fmt.Errorf(color.InRed("configFile param is empty"), "")
+		return conf, fmt.Errorf(color.InRed("configFile param is empty"), "")
 	}
-	fmt.Println(color.InYellow(">>>> [INFO] ConfigFile param is not empty. Reading file from this path: " + configPath))
-	config.ConfigFile = configPath
-	// Set the rest of config from env
-	if kubeconfig == "" {
-		return config, fmt.Errorf(color.InRed("Kubeconfig param is empty"), "")
-	}
-	fmt.Println(color.InYellow(">>>> [INFO] KUBECONFIG env is not empty. Reading file from this path: " + kubeconfig))
-	config.Kubeconfig = kubeconfig
-
 	//Read config from file
-	err := config.readFromConfigFile()
+	f, err := os.Open(configPath)
+	if err != nil {
+		return conf, fmt.Errorf(color.InRed("opening config file %s: %v"), conf.ConfigFile, err)
+	}
+
+	decoder := yaml.NewDecoder(f)
+	if err := decoder.Decode(conf); err != nil {
+		return conf, fmt.Errorf(color.InRed("decoding config file %s: %v"), conf.ConfigFile, err)
+	}
 	if err != nil {
 		fmt.Println(color.InRed(">>>> [ERROR] Error reading config file: " + err.Error()))
-		return config, err
+		return conf, err
 	}
+	fmt.Println(color.InYellow(">>>> [INFO] ConfigFile param is not empty. Reading file from this path: " + configPath))
+
+	// Set the rest of config from env
+	if kubeconfig == "" {
+		return conf, fmt.Errorf(color.InRed("Kubeconfig param is empty"), "")
+	}
+	fmt.Println(color.InYellow(">>>> [INFO] KUBECONFIG env is not empty. Reading file from this path: " + kubeconfig))
+	conf.Kubeconfig = kubeconfig
 
 	//modify config for source index depending on the config read from file
-	config.RegistryOLMSourceIndex += strings.Join(strings.Split(config.RegistryOCPRelease, ".")[:2], ".")
+	conf.RegistryOLMSourceIndex += strings.Join(strings.Split(conf.RegistryOCPRelease, ".")[:2], ".")
 
-	return config, nil
+	return conf, nil
 }
 
 //ReadFromConfigFile reads the config file
